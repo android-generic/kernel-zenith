f7e178181e5fbde5d7df95578b013b258c8772a1
Orlando Chamberlain <orlandoch.dev@gmail.com>
2023-02-18 23:05:05 +1100

    i915: 4 lane quirk for mbp15,1
    Needed to use iGPU when dGPU was boot GPU
    
    Patch written by Kerem Karabay <kekrby@gmail.com>

--- drivers/gpu/drm/i915/display/intel_ddi.c
+++ drivers/gpu/drm/i915/display/intel_ddi.c
@@ -4652,6 +4652,9 @@ static bool intel_ddi_a_force_4_lanes(struct intel_digital_port *dig_port)
 	if (dig_port->saved_port_bits & DDI_A_4_LANES)
 		return false;
 
+	if (intel_has_quirk(dev_priv, QUIRK_DDI_A_FORCE_4_LANES))
+		return true;
+
 	/* Broxton/Geminilake: Bspec says that DDI_A_4_LANES is the only
 	 *                     supported configuration
 	 */

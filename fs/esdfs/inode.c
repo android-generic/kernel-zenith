680de364eb77cbacbc9bc0fd1ef444ede2053767
hmtheboy154 <buingoc67@gmail.com>
2026-02-25 14:29:46 -0500

    esdfs: VFS: unify old_mnt_idmap and new_mnt_idmap in renamedata
    Based on this commit https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?h=v6.18&id=d7fb2c410240348edee7867c29b60688175dcc11
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- fs/esdfs/inode.c
+++ fs/esdfs/inode.c
@@ -303,11 +303,10 @@ static int esdfs_rename(struct mnt_idmap *idmap,
 		goto out;
 	}
 
-	rd.old_mnt_idmap = idmap;
-	rd.old_parent = lower_old_dir_dentry->d_inode;
+	rd.mnt_idmap = idmap;
+	rd.old_parent = lower_old_dir_dentry;
 	rd.old_dentry = lower_old_dentry;
-	rd.new_mnt_idmap = idmap;
-	rd.new_parent = lower_new_dir_dentry->d_inode;
+	rd.new_parent = lower_new_dir_dentry;
 	rd.new_dentry = lower_new_dentry;
 	rd.flags = flags;
  
